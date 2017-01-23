b1182e86078f860ea14a8160042352907c205353
Nia <nia.williams@gmail.com>
2017-01-23 02:22:51 -0600

    tweaks pre-responsive css, form needs fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,23 @@
+// Responsive hamburger
+
 $('.hamburger').on('click', function(){
     // $('.sidebar').toggleClass('active-sidebar');
     $(this).toggleClass('active');
     $('nav').toggle()
+});
+
+// Hero Slider
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+	
+
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
 });
