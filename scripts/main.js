7aced4d6c4cd2a2a15b0b5c9464bbbf2c1d70266
Nia <nia.williams@gmail.com>
2017-01-24 18:18:21 -0600

    final presentation but not final form

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,21 @@ $('.hamburger').on('click', function(){
 });
 
 // Hero Slider
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
-});
+// $(document).ready(function(){
+//   $('.fade').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: 'linear'
+// });
 	
 
-$('.fade').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-});
+
+
+// Mobile or responsive hero and intro
+// function shrinkHero (){
+// 	if ($('#hero-container').css('background-color') === '#e5e5e5' ) {
+// 		$('.banner-responsive p').html('<strong>Identify challenges. Find solutions. Gain insights. Make smart decisions.</strong><p>Hospital Forum connects health care leaders with data, education, tools and networking opportunities to help them achieve performance excellence and provide exceptional care in their communities.</p>')
+// 	}
+// }
